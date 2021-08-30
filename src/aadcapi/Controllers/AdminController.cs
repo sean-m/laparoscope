d44afd4206089b10d01b24bb8a1514d5a690cbaf
Sean McArdle <sean@mcardletech.com>
2021-08-30 16:14:29 -0700

    Updated too bootstrap 4.6.

--- src/aadcapi/Controllers/AdminController.cs
+++ src/aadcapi/Controllers/AdminController.cs
@@ -6,11 +6,13 @@ using System.Web.Mvc;
 
 namespace aadcapi.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         // GET: Admin
         public ActionResult Index()
         {
+            ViewBag.GlobalProps = typeof(aadcapi.Utils.Globals).GetProperties().OrderBy(x => x.Name);
             return View();
         }
     }
