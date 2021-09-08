2b891b02cdc1a8a9fe66fb9153b59acfc210cd71
Sean McArdle <sean@mcardletech.com>
2021-09-08 13:32:52 -0700

    Added unit tests for the psobject conversion extension.

---
+++ src/aadcapi.UnitTests/PSObjectUnitTests.cs
@@ -0,0 +1,55 @@
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Management.Automation;
+using static SMM.Helper.Extensions;
+
+namespace aadcapi.UnitTests
+{
+    public class PSObjectExtension
+    {
+        public class FooTest
+        {
+            public string Foo { get; set; }
+            public string FooBar { get; set; }
+            public FooTest Nested { get; set; }
+            public FooTest() { }
+        }
+
+        FooTest result;
+
+        [SetUp]
+        public void Setup()
+        {
+            var runner = new SMM.Automation.SimpleScriptRunner("[PSCustomObject]@{'Foo'='Bar';'FooBar'='Baz';Nested=[PSCustomObject]@{'Foo'='Bar';'FooBar'='Baz';}}");
+            runner.Run();
+            result = runner.Results.CapturePSResult<FooTest>().Cast<FooTest>().FirstOrDefault();
+        }
+
+        [Test]
+        public void TestCapturedTypeNotNull()
+        {
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void TestCapturedTypeCastCorrectly()
+        {
+            Assert.IsInstanceOf<FooTest>(result, "Did not capture PSCustomObject as FooTest.");
+        }
+
+        [Test]
+        public void TestCapturedProperties()
+        {
+            Assert.AreEqual(result.Foo, "Bar");
+            Assert.AreEqual(result.FooBar, "Baz");
+        }
+
+        [Test]
+        public void TestNestedCapture()
+        {
+            Assert.IsNotNull(result.Nested);
+            Assert.IsInstanceOf<FooTest>(result.Nested);
+        }
+    }
+}
