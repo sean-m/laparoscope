48ad7e3f6c31db1948df923a63fde5359efebc8a
Sean McArdle <sean@mcardletech.com>
2021-09-10 16:11:33 -0700

    Added table to view existing authorization rules.

--- src/aadcapi/Controllers/AdminController.cs
+++ src/aadcapi/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using aadcapi.Utils.Authorization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,8 @@ namespace aadcapi.Controllers
         public ActionResult Index()
         {
             ViewBag.GlobalProps = typeof(aadcapi.Utils.Globals).GetProperties().OrderBy(x => x.Name);
+
+            ViewBag.AuthorizationRules = RegisteredRoleControllerRules.GetRoleControllerModels();
             return View();
         }
     }
