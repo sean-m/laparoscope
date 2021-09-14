1ddcb1a0c97361c80676572efca79f1303f1d24f
Sean McArdle <sean@mcardletech.com>
2021-09-13 17:22:33 -0700

    Added xmldoc comment and filtering for setting secrets via api.

--- src/aadcapi/Controllers/ConfigController.cs
+++ src/aadcapi/Controllers/ConfigController.cs
@@ -16,13 +16,22 @@ namespace aadcapi.Controllers
     {
         private readonly Type _global_t = typeof(Globals);
 
-        public dynamic Get(string Name = null)
+        /// <summary>
+        /// Endpoint for querying runtime application settings. Settings can be specified by
+        /// the Setting query parameter as a single setting name or comma separated list of names.
+        /// Wildcards are supported. If no Setting is specified, an empty set is returned. You can
+        /// return a single setting name by route /api/Config/[Setting]. If both are specified,
+        /// query string wins.
+        /// </summary>
+        /// <param name="Setting">Single or comma separated list of setting names.</param>
+        /// <returns>Array of 0 or more values: [ { Setting:name, Value:value } ].</returns>
+        public dynamic Get(string Setting = null)
         {
             // Only users of the role specified as allowed to access config via the api may call this (Admin by default).
             if (!this.RequestContext.Principal.IsInRole(Globals.AuthorizedConfigRole)) { return Unauthorized(); }
 
-            var nameOrId = String.IsNullOrEmpty(Name) ? this.RequestContext.RouteData.Values["id"]?.ToString() ?? String.Empty : Name.TrimStart('\'', '"').TrimEnd('\'', '"'); 
-            var names = String.IsNullOrEmpty(nameOrId) ? new string[] { nameOrId } : nameOrId.Split(',');
+            var nameOrId = String.IsNullOrEmpty(Setting) ? this.RequestContext.RouteData.Values["id"]?.ToString() ?? String.Empty : Setting.TrimStart('\'', '"').TrimEnd('\'', '"');
+            var names = String.IsNullOrEmpty(nameOrId) ? new string[] { nameOrId } : nameOrId?.Split(',')?.Select(x => x.Trim());
 
             var appSettings = ConfigurationManager.AppSettings.AllKeys;
             var appSettingsResult = appSettings.Where(k => names.Any(n => k.Like(n))).Select(k =>  new PropertyValue { Setting = k, Value = k.Like("*Secret*") ? "*****************" : ConfigurationManager.AppSettings[k]});
@@ -32,12 +41,20 @@ namespace aadcapi.Controllers
             return Ok(Enumerable.Concat(appSettingsResult, globalsResult).DistinctBy(x => x.Setting));
         }
 
-        public dynamic Put(string Name, string Value)
+        public dynamic Put(string Setting, string Value)
         {
             // Only users of the role specified as allowed to access config via the api may call this (Admin by default).
             if (!this.RequestContext.Principal.IsInRole(Globals.AuthorizedConfigRole)) { return Unauthorized(); }
 
             // TODO (Sean) Handle incoming Rule.* settings.
+            
+            // Changes to secrets are not supported via the api. Should be automated with
+            // CI/CD or secure credential storage: Azure Key Vault, etc.
+            if (Setting.Like("*Secret*")) {
+                var response = new HttpResponseMessage(HttpStatusCode.MethodNotAllowed);
+                return response;
+            }
+
 
             return Ok();
         }
