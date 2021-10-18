3b34aacb02021a0a2633d0fe735933043aacc7b3
Sean McArdle <sean@mcardletech.com>
2021-10-18 10:23:19 -0700

    Added Get-ADSyncAutoUpgrade.

---
+++ src/aadcapi/Controllers/Server/AutoUpgradeController.cs
@@ -0,0 +1,30 @@
+﻿using SMM.Automation;
+using SMM.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace aadcapi.Controllers.Server
+{
+    [Authorize]
+    public class AutoUpgradeController : ApiController
+    {
+        public dynamic Get()
+        {
+            var runner = new SimpleScriptRunner("Get-ADSyncAutoUpgrade");
+            runner.Run();
+
+            if (runner.HadErrors)
+            {
+                var err = runner.LastError ?? new Exception("Encountered an error in PowerShell but could not capture the exception.");
+                return InternalServerError(err);
+            }
+
+            var result = Ok(runner.Results.ToDict().FirstOrDefault());
+            return result;
+        }
+    }
+}
