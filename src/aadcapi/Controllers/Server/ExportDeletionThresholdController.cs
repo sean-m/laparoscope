01cf5ab0b77acebaa590e22b12f9900de42bc308
Sean McArdle <sean@mcardletech.com>
2021-10-18 11:31:30 -0700

    Added Get-ADSyncExportDeletionThreshold.

---
+++ src/aadcapi/Controllers/Server/ExportDeletionThresholdController.cs
@@ -0,0 +1,34 @@
+﻿using SMM.Automation;
+using SMM.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace aadcapi.Controllers.Server
+{
+    [Authorize]
+    public class ExportDeletionThresholdController : ApiController
+    {
+        /// <summary>
+        /// Maps to Get-ADSyncExportDeletionThreshold cmdlet. This gives enabled/disabled (1/0)
+        /// status and the numeric value of the current threshold.
+        /// </summary>
+        public dynamic Get()
+        {
+            var runner = new SimpleScriptRunner("Get-ADSyncExportDeletionThreshold");
+            runner.Run();
+
+            if (runner.HadErrors)
+            {
+                var err = runner.LastError ?? new Exception("Encountered an error in PowerShell but could not capture the exception.");
+                return InternalServerError(err);
+            }
+
+            var result = Ok(runner.Results.ToDict().FirstOrDefault());
+            return result;
+        }
+    }
+}
