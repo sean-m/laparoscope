f685e20790df4dad09bce6adc951ca9bb24d2240
Sean McArdle <sean@mcardletech.com>
2021-11-03 21:28:55 -0700

    Started working on UI for admins.

---
+++ src/aadcapi/Controllers/Server/ServerController.cs
@@ -0,0 +1,21 @@
+﻿using aadcapi.Utils.Authorization;
+using McAuthorization;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aadcapi.Controllers
+{
+    [Authorize]
+    public class ServerController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
