566f4ed7ae88b3f7534da86ab4d5141639ae4644
Sean McArdle <sean@mcardletech.com>
2022-03-08 09:01:08 -0800

    Added JwtBearerAuthentication. Still need to do token validation.

---
+++ src/aadcapi/App_Start/Startup.JwtAuth.cs
@@ -0,0 +1,48 @@
+﻿
+using aadcapi.Utils;
+using Auth0.Owin;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Jwt;
+using Owin;
+
+namespace aadcapi
+{
+    public partial class Startup
+	{
+		private void ConfigureJwtAuth(IAppBuilder app) {
+			
+			app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+			app.UseCookieAuthentication(new CookieAuthenticationOptions { });
+
+			string authority = Globals.Authority;
+			string clientId = Globals.ClientId;
+			string secret = Globals.ClientSecret;
+			string audience = Globals.RedirectUri;
+
+			var signingKey = new SymmetricSecurityKey(System.Text.Encoding.ASCII.GetBytes(secret));
+
+			var keyResolver = new OpenIdConnectSigningKeyResolver(authority);
+
+			var options = new JwtBearerAuthenticationOptions
+			{
+				AuthenticationMode = AuthenticationMode.Active,
+				AllowedAudiences = new string[] { audience },
+				TokenValidationParameters = new TokenValidationParameters
+				{
+					RequireExpirationTime = true,
+					ValidateLifetime = true,
+					RequireSignedTokens = false,
+					ValidateIssuerSigningKey = false,
+					ValidateIssuer = false,
+					ValidateAudience = false,
+					IssuerSigningKeyResolver = (token, securityToken, kid, parameters) => keyResolver.GetSigningKey(kid)
+				},
+			};
+
+			app.UseJwtBearerAuthentication(options);
+		}
+	}
+}
