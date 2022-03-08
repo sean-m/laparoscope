5a1d5cadb8bc557769071b6d266c4c49c0f29f3e
Sean McArdle <sean@mcardletech.com>
2022-03-08 10:00:24 -0800

    Have to serialize and deserialize the ClaimsIdentity object because it has circular references to itself...I hope there was a good reason for that.

--- src/aadcapi/Controllers/MeController.cs
+++ src/aadcapi/Controllers/MeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
@@ -15,6 +16,19 @@ namespace aadcapi.Controllers
         {
             ViewBag.UserId = (ClaimsIdentity) HttpContext.User.Identity;
             return View();
+        }
+    }
+
+    [System.Web.Http.Authorize]
+    public class MeApiController : System.Web.Http.ApiController
+    {
+        public dynamic Get()
+        {
+            var json = JsonConvert.SerializeObject(
+                (ClaimsIdentity)RequestContext.Principal.Identity,
+                new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore }
+                );
+            return Ok( JsonConvert.DeserializeObject<Dictionary<string, object>>(json));
         }
     }
 }
