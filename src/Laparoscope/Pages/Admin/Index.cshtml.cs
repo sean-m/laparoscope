c654e84a3512fa8d558ba65a4ffc828538e6a3c5
Sean McArdle <sean@mcardletech.com>
2024-10-01 08:44:49 -0700

    Admin and Me pages are working.

---
+++ src/Laparoscope/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using McAuthorization;
+using McAuthorization.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+
+namespace Laparoscope.Pages.Admin
+{
+    public class IndexModel : PageModel
+    {
+
+        //IEnumerable<object> GlobalProps = typeof(aadcapi.Utils.Globals).GetProperties().OrderBy(x => x.Name);
+
+        //ViewBag.AppSettings = ConfigurationManager.AppSettings;
+        //ViewBag.SettingKeys = ConfigurationManager.AppSettings.AllKeys.OrderBy(x => x);
+        internal IEnumerable<RoleFilterModel> AuthorizationRules { get ; set; }
+
+        public void OnGet()
+        {
+            AuthorizationRules = RegisteredRoleControllerRules.GetRoleControllerModels();
+        }
+    }
+}
---
+++ src/Laparoscope/Pages/Me/Index.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+
+namespace Laparoscope.Pages.Me
+{
+    public class IndexModel : PageModel
+    {
+        ClaimsPrincipal User { get; set; }
+
+        public void OnGet()
+        {
+            User = PageContext.HttpContext.User;
+        }
+    }
+}
