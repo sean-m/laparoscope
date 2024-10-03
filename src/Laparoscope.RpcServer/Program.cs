54cf64ac88fef08ec5e081fa6f282f56bcdeb15c
Sean McArdle <sean@mcardletech.com>
2024-10-03 11:17:53 -0700

    Added checks to disallow running while host allows anonymous access to named pipes.

--- src/Laparoscope.RpcServer/Program.cs
+++ src/Laparoscope.RpcServer/Program.cs
@@ -1,12 +1,16 @@
 using Laparoscope.RpcServer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace App
 {
     public static class Program {
         public static void Main(string[] args) {
             var builder = Host.CreateApplicationBuilder(args);
+            builder.Services.AddLogging(option => {
+                option.AddConsole();
+            });
             builder.Services.AddHostedService<Worker>();
 
             var host = builder.Build();
--- src/Laparoscope.RpcServer/Worker.cs
+++ src/Laparoscope.RpcServer/Worker.cs
@@ -3,12 +3,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Win32;
 using StreamJsonRpc;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Pipes;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -19,6 +21,8 @@ namespace Laparoscope.RpcServer {
 
         public Worker(ILogger<Worker> logger) {
             _logger = logger;
+
+            RunSafetyChecks();
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
@@ -26,29 +30,55 @@ namespace Laparoscope.RpcServer {
                 if (_logger.IsEnabled(LogLevel.Information)) {
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 }
-                await NamedPipeServerAsync();
+                await NamedPipeServerAsync(stoppingToken);
             }
         }
 
 
-        private async Task NamedPipeServerAsync() {
+        private async Task NamedPipeServerAsync(CancellationToken token) {
             int clientId = 0;
             while (true) {
                 _logger.LogInformation ("Waiting for client to make a connection...");
                 var stream = new NamedPipeServerStream("Laparoscope", PipeDirection.InOut, 
                     NamedPipeServerStream.MaxAllowedServerInstances, 
                     PipeTransmissionMode.Byte, PipeOptions.Asynchronous);
-                await stream.WaitForConnectionAsync();
+                await stream.WaitForConnectionAsync(token);
                 Task nowait = RespondToRpcRequestsAsync(stream, ++clientId);
             }
         }
 
         private async Task RespondToRpcRequestsAsync(Stream stream, int clientId) {
             _logger.LogInformation($"Connection request {clientId} received.");
+
+
+
             var jsonRpc = JsonRpc.Attach(stream, new ServerActions());
             _logger.LogInformation($"JSON-RPC listener attached to {clientId}. Waiting for requests...");
             await jsonRpc.Completion;
             _logger.LogInformation($"Connection {clientId} terminated.");
+        }
+
+        private void RunSafetyChecks()
+        {
+            // Check the RestrictNullSessAccess key
+            string lanmanParameters = "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Services\\LanManServer\\Parameters";
+            var desiredValue = 1;
+            object regValue = Registry.GetValue(lanmanParameters, "RestrictNullSessAccess", desiredValue);
+            if (regValue == null || (int)regValue != desiredValue)
+            {
+                _logger?.LogWarning($"RestrictNullSessAccess check failed! Expected value: {desiredValue}, discovered value: {regValue ?? "NULL"}.");
+                throw new Exception($"RestrictNullSessAccess check failed! Expected value: {desiredValue}, discovered value: {regValue ?? "NULL"}.");
+            }
+
+            // Verify the pipe name is not in the NullSessionPipes list
+            object pipeList = Registry.GetValue(lanmanParameters, "NullSessionPipes", new string[] { });
+            if (pipeList is string[] nullPipes)
+            {
+                if (nullPipes.Any(x => x.Equals("Laparoscope", StringComparison.CurrentCultureIgnoreCase))) {
+                    _logger.LogWarning($"NullSessionPipes includes 'Laparoscope' allowing anonymous access to the pipe. Panic!");
+                    throw new Exception("Will not run with anonymous access to named pipe. Remove name from NullSessionPipes.");
+                }
+            }
         }
     }
 }
