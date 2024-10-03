37c433a77e84296ec0e338a94b6bac485d609303
Sean McArdle <sean@mcardletech.com>
2024-10-03 11:18:24 -0700

    Added controller for checking RPC sidecar service status.

---
+++ src/Laparoscope/Controllers/SidecarStatusController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Threading;
+using System.IO.Pipes;
+
+namespace Laparoscope.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class SidecarStatusController : ControllerBase
+    {
+        ILogger logger;
+        public SidecarStatusController(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<dynamic> GetAsync(int timeout=20)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            var result = new SidecarStatus();
+
+            watch.Start();
+            try
+            {
+                using (var stream = new NamedPipeClientStream(".", "Laparoscope", PipeDirection.InOut, PipeOptions.Asynchronous))
+                {
+                    await stream.ConnectAsync().WithTimeout(TimeSpan.FromSeconds(timeout));
+                    watch.Stop();
+                }
+            }
+            catch (TimeoutException te)
+            {
+                result.PipeStatus = "Timeout";
+                result.Error = te.Message;
+                logger.LogError(te, "Timeout exception.");
+            }
+            catch (Exception ex)
+            {
+                result.PipeStatus = "Error";
+                result.Error = ex.Message;
+                logger.LogError(ex, ex.Message);
+            }
+            finally
+            {
+                if (watch.IsRunning) watch.Stop();
+                result.ConnectionLatency = watch.Elapsed.TotalSeconds.ToString();
+            }
+
+            return result;
+        }
+    }
+
+    public class SidecarStatus
+    {
+        public string PipeStatus { get; set; } = "Active";
+        public string ConnectionLatency { get; set; }
+        public string Error { get; set; }
+    }
+}
