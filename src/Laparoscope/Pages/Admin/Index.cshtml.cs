bc7d0d926429236b160bc7747d04297d20f4af37
Sean McArdle <sean@mcardletech.com>
2026-05-11 18:37:13 -0700

    Fixed the configuration not loading. That's the last feature lost during the asp.net core migration.

--- src/Laparoscope/Pages/Admin/Index.cshtml.cs
+++ src/Laparoscope/Pages/Admin/Index.cshtml.cs
@@ -3,25 +3,81 @@ using LaparoscopeShared.Models;
 using McAuthorization;
 using McAuthorization.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using System.Security.Claims;
+using System.Linq;
+using Laparoscope.Controllers.Server;
 
 namespace Laparoscope.Pages.Admin
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _configuration;
 
-        //IEnumerable<object> GlobalProps = typeof(aadcapi.Utils.Globals).GetProperties().OrderBy(x => x.Name);
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
-        //ViewBag.AppSettings = ConfigurationManager.AppSettings;
-        //ViewBag.SettingKeys = ConfigurationManager.AppSettings.AllKeys.OrderBy(x => x);
-        public IEnumerable<RoleFilterModel> AuthorizationRules { get ; set; } = Array.Empty<RoleFilterModel>();
+        public IEnumerable<RoleFilterModel> AuthorizationRules { get; set; } = Array.Empty<RoleFilterModel>();
 
         public IEnumerable<WindowsTask> Processes { get; set; }
 
-        public async void OnGet()
+        public IEnumerable<Setting> AppSettings { get; set; } = new List<Setting>();
+
+        public void OnGet()
         {
             AuthorizationRules = RegisteredRoleControllerRules.GetRoleControllerModels();
-            Processes = await ProcessesController.GetProcesses();
+            Processes = ProcessesController.GetProcesses().GetAwaiter().GetResult();
+
+            // Recursively walk configuration and build path-based dictionary
+            AppSettings = FlattenConfiguration(_configuration)
+                .Select(x => x.Value);
+        }
+
+        private Dictionary<string, Setting> FlattenConfiguration(IConfiguration config)
+        {
+            var result = new Dictionary<string, Setting>();
+            if (config is IConfigurationRoot root)
+            {
+                foreach (var provider in root.Providers.Reverse())
+                {
+                    var providerName = provider.GetType().Name;
+
+                    var keys = provider.GetChildKeys(Enumerable.Empty<string>(), null);
+                    foreach (var k in keys)
+                    {
+                        // Fix for CS8601: Ensure v is not null
+                        var value = provider.TryGet(k, out var v) && v != null ? v : string.Empty;
+                        if (!string.IsNullOrEmpty(value)) 
+                            result[k] = new Setting() { Key = k, Value = value, Source = providerName };
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public class Setting
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        private string _source;
+        public string Source { get => GetSourceName(_source); set => _source = value; }
+
+        public string GetSourceName(string source)
+        {
+            return source switch
+            {
+                "CommandLine" => "cli",
+                "EnvironmentVariablesConfigurationProvider" => "env",
+                "UserSecrets" => "userSecret",
+                "AzureKeyVault" => "azkv",
+                "AzureAppConfigurationProvider" => "azappconfig",
+                "JsonConfigurationProvider" => "json",
+                _ => source
+            };
         }
     }
 }
